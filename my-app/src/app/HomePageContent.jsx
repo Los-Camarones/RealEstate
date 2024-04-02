0ab5b256663e6d14ceee609474c908477c4ecef4
MANSOURCSS <Mohamedahmed2@CSUS.EDU>
2024-04-01 17:58:06 -0700

    changed the background to a photo and made the "About Me" section look nicer.

--- my-app/src/app/HomePageContent.jsx
+++ my-app/src/app/HomePageContent.jsx
@@ -1,28 +1,27 @@
 import React from 'react';
-import './globals.css';
+import './HomePageContent.css';
+
 const HomePageContent = () => {
   return (
     <div className="homepage-content">
       <section className="about-me">
-        <h1>About Lourdes Mendoza</h1>
+         <h1>About Lourdes Mendoza</h1>
         <p>
-          I consider myself extremely fortunate to be doing exactly what I want in life. In my case, this means
-          working with my family and friends, friends of friends, and other personal referrals to buy and sell real
-          estate in the greater Sacramento area, as well as in Sutter, Roseville and Yuba Counties. I lived in Maryville
-          and graduated from Lindhurst HS. At 115, I was off to CSU Sacramento where I obtained a Communication Studies
-          degree. Coming from an agricultural, migrant background, my parents did not have the means to pay for my college
-          education so I put myself through school by working in the fields during summer break and then by working as a
-          student assistant. While in college, I experienced my greatest life success. I was able to travel abroad and
-          live in Sweden as an international student for one year. One of the most rewarding experiences. I knew then that
-          anything was possible. I am very proud to live in a different culture and to be able to adapt so well. The
-          highlight of my year abroad was traveling in St. Petersburg, Russia.
-          I enjoy being a Realtor because I love working closely with individuals and learning about their goals and
-          dreams of homeownership. I am excited to assist individuals in attaining their dream of homeownership. The most
-          important thing to know is that I am a professional and I seek education in order to stay current on the ever
-          changing Real Estate market. I would never expect to be given your business because of our relationship, or how
-          we may know each other. However, if you ever have a real estate need, I would be extremely honored if you
-          would give me an opportunity to work for you to earn your business. Thank you so much for taking the time to
-          read a little about me, it is appreciated.
+        I consider myself extremely fortunate to be doing exactly what I want to do in life. In my case this means working with my family 
+        and friends, friends of my friends, and other personal referrals to buy and sell real estate in the greater Sacramento area, as 
+        well as in Sutter, Yolo and Yuba Counties. I lived in Marysville until I graduated from Lindhurst H.S. After H.S., I was off to
+        CSU Sacramento where I obtained a B.S. in Communication Studies. Coming from an agricultural, migrant background, my parents did 
+        not have the means to pay for my college education so I put myself through school by working in the fields during summer break and
+        then by working as a student assistant. While in college, I experienced my first major success in life. I was able to travel
+        abroad and live in Sweden as an international student for one year. One of the most rewarding experiences! I knew then that
+        anything was possible. I felt very proud to live in a different culture and to be able to adapt so well. The highlight of my 
+        year abroad was traveling to St. Petersburg, Russia. I enjoy being a Realtor because I love working closely with individuals
+        and learning about their goals and dreams of home ownership. I am excited to assist individuals in attaining their goal of
+        homeownership. The most important thing to know is, I am professional and I seek education in order to stay current on an 
+        ever changing Real Estate market. I would of never expect to be given your business because of our relationship, or how we
+        may know each other. However, if you ever have a residential real estate need, I would be extremely honored if you would 
+        give me an opportunity to work hard for you to earn your business. Thank you so much for taking the time read a little 
+        about me. It is appreciated.
         </p>
       </section>
     </div>
--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Image from 'next/image';
-import NavBar from './navbar'; 
-import HomePageContent from './HomePageContent'; 
+import NavBar from './navbar';
+import HomePageContent from './HomePageContent';
+import "./globals.css";
 
 function Page() {
   return (
@@ -15,12 +16,33 @@ function Page() {
         <NavBar />
       </div>
 
-      <div className="home-content-wrapper"> {/* Wrapper for content below navbar */}
-        <HomePageContent />
+      {/* Background photo */}
+      <div style={{
+        backgroundImage: "url('/MyBR.jpg')", // background image
+        backgroundSize: 'cover', // Cover the entire area of the div
+        backgroundPosition: 'center', // Center the background image
+        height: '100vh', // Make it take the full height of the viewport
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}>
+        {/* Text Overlay */}
+        <div className="overlay-content" style={{ textAlign: 'center', color: '#fff' }}> {/* Adjust the color based on your background */}
+          <h1>Transform your Dreams Into a Luxurious Address!</h1>
+          <h2>Lourdes Mendoza</h2>
+          <h3>The 0007 Real Estate Agent</h3>
+          <p>Powered by BIG BLOCK REALTY NORTH</p>
+          </div>
       </div>
+
+      {/* About Lourdes Section */}
+      <HomePageContent />
+
+      {/* Rest of the page content */}
     </div>
   );
 }
 
 export default Page;
+
 
