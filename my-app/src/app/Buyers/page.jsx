cd198e6dc0459afa721ba3b122fe37aacc472f4d
Xxuan002_school <157923048+xuanxuan002@users.noreply.github.com>
2024-04-02 18:29:51 -0700

    workign  pages without link
    this is page without Links

---
+++ my-app/src/app/Buyers/page.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const AboutMe = () => {
+    return (
+      <div>
+        <h1>Show houses that are trending on market.</h1>
+        
+        <p> detail from metralist plzzz give me the API detailllllllahhhhhh
+        </p>
+      </div>
+    );
+  }
+  
+  export default AboutMe;
---
+++ my-app/src/app/Properties/page.jsx
@@ -0,0 +1,13 @@
+
+
+
+const Properties = () => {
+    return (
+      <div>
+        <h1>View More Properties</h1>
+        <p> list of properties thats corruntly under mangament of Lourdez. </p>
+      </div>
+    );
+  }
+  
+  export default Properties;
