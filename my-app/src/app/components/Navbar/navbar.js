18a1ecfea3866673ff33a9c5336f857d56ec8cfe
ShadeManEXE <jflores99@rocketmail.com>
2024-04-07 23:04:01 -0700

    DP-67 Implement Authentication UI in Header Bar

--- my-app/src/app/components/Navbar/navbar.js
+++ my-app/src/app/components/Navbar/navbar.js
@@ -1,43 +1,56 @@
 import React from 'react';
 import Link from 'next/link';
 
+//This style is the style of the buttons on the navbar
+//TODO: We need a better way to manage all styles so they are easily reusable in other pages without having to retype them
+
+styles.buttonStyle = "ml-2 bg-gray-500 hover:bg-gray-700 text-white font-bold py-1 px-2 rounded";
 
 function NavBar() {
   return (
     <div className="flex items-center bg-gray-500">
-      <button className="ml-2 bg-gray-500 hover:bg-gray-700 text-white font-bold py-1 px-2 rounded">
+      <button className={buttonStyleTEST}>
         Home
       </button>
 
-      <button className="ml-2 bg-gray-500 hover:bg-gray-700 text-white font-bold py-1 px-2 rounded">
+      <button className={styles.buttonStyle}>
         Listings
       </button>
 
       <Link href = "/about">
-      <button className="ml-2 bg-gray-500 hover:bg-gray-700 text-white font-bold py-1 px-2 rounded">
+      <button className={styles.buttonStyle}>
        About Me
       </button>
       </Link>
 
-      <button className="ml-2 bg-gray-500 hover:bg-gray-700 text-white font-bold py-1 px-2 rounded">
+      <button className={styles.buttonStyle}>
         Sellers
       </button>
 
-      <button className="ml-2 bg-gray-500 hover:bg-gray-700 text-white font-bold py-1 px-2 rounded">
+      <button className={styles.buttonStyle}>
         Buyers
       </button>
 
-        <button className="ml-2 bg-gray-500 hover:bg-gray-700 text-white font-bold py-1 px-2 rounded">
+      <button className={styles.buttonStyle}>
             Communities
         </button>
 
-        <button className="ml-2 bg-gray-500 hover:bg-gray-700 text-white font-bold py-1 px-2 rounded">
+        <button className={styles.buttonStyle}>
             Properties
         </button>
 
-        <button className="ml-2 bg-gray-500 hover:bg-gray-700 text-white font-bold py-1 px-2 rounded">
+        <button className={styles.buttonStyle}>
             What's My Home Worth?
         </button>
+
+        <link href = "/sign-in">
+        <button  className={styles.buttonStyle}>
+            Sign Up
+        </button>
+        </link>
+
+        
+
         
     </div>
   );
