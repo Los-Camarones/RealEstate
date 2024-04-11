639213ef937b92b8cde0d595e3608a2f92f489a5
Xxuan002_school <157923048+xuanxuan002@users.noreply.github.com>
2024-04-10 22:45:47 -0700

    # Dp-78 and 89 repush
    thrid push attampt

--- my-app/src/app/components/Navbar/navbar.tsx
+++ my-app/src/app/components/Navbar/navbar.tsx
@@ -8,8 +8,6 @@ const buttonStyle = "ml-2 bg-gray-500 hover:bg-gray-700 text-white font-bold py-
 function NavBar() {
   return (
     <div className="flex items-center bg-gray-500">
-<<<<<<< HEAD:my-app/src/app/components/Navbar/navbar.js
-
 
       <Link href="/"> 
       <button className={buttonStyle}>
@@ -19,33 +17,24 @@ function NavBar() {
 
 
 
-      <Link href="/Listings">
-=======
-    <Link href="http://localhost:3000/">
-        <button className={buttonStyle}>
-        Home
-      </button>
-    </Link>
 
-      <Link href="/Listings/page">
->>>>>>> 80a5d3b7e2e8e8fc4d0e0ffe0d8522e514056973:my-app/src/app/components/Navbar/navbar.tsx
+
+
+      <Link href="/Listings">
       <button className={buttonStyle}>
         Listings
       </button>
       </Link>
 
-<<<<<<< HEAD:my-app/src/app/components/Navbar/navbar.js
 
 
       <Link href="/Aboutme">
-=======
-      <Link href="/Aboutme/page">
->>>>>>> 80a5d3b7e2e8e8fc4d0e0ffe0d8522e514056973:my-app/src/app/components/Navbar/navbar.tsx
         <button className={buttonStyle}>
           About Me
         </button>
       </Link>
-<<<<<<< HEAD:my-app/src/app/components/Navbar/navbar.js
+
+
 
     
 
@@ -67,65 +56,34 @@ function NavBar() {
 
 
 
+
       <Link href="/Communities">
-=======
-    <Link href="/Sellers/page">
-      <button className={buttonStyle}>
-        Sellers
-      </button>
-    </Link>
-
-      <Link href="/Buyers/page">
-        <button className={buttonStyle}>
-          Buyers
-        </button>
-      </Link>
-
-      <Link href="/Communities/page">
->>>>>>> 80a5d3b7e2e8e8fc4d0e0ffe0d8522e514056973:my-app/src/app/components/Navbar/navbar.tsx
       <button className={buttonStyle}>
         Communities
       </button>
       </Link>
 
-<<<<<<< HEAD:my-app/src/app/components/Navbar/navbar.js
 
 
 
       <Link href="/Properties">
-=======
-      <Link href="/Properties/page">      
->>>>>>> 80a5d3b7e2e8e8fc4d0e0ffe0d8522e514056973:my-app/src/app/components/Navbar/navbar.tsx
       <button className={buttonStyle}>
         Properties
       </button>
       </Link>
 
-<<<<<<< HEAD:my-app/src/app/components/Navbar/navbar.js
-
-
 
       <Link  href="/Homeworth">
-=======
-      <Link href="/WhatMyHomeWorth/page">
->>>>>>> 80a5d3b7e2e8e8fc4d0e0ffe0d8522e514056973:my-app/src/app/components/Navbar/navbar.tsx
       <button className={buttonStyle}>
         What's My Home Worth?
       </button>
       </Link>
-
-<<<<<<< HEAD:my-app/src/app/components/Navbar/navbar.js
 
 
 
       <Link href="/Login">
         <button className={buttonStyle}>
           Login
-=======
-      <Link href="/Sign-in/page">
-        <button className={buttonStyle}>
-          Sign-in
->>>>>>> 80a5d3b7e2e8e8fc4d0e0ffe0d8522e514056973:my-app/src/app/components/Navbar/navbar.tsx
         </button>
       </Link>
 
