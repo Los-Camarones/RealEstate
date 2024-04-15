73d9a4c0bc0bab04b4c0a0a0916a1da0eb24dce7
Eric <95461143+ERC-320-61@users.noreply.github.com>
2024-04-14 17:29:59 -0700

    Adjusted Values to mirror my manual EC2 setup

--- CI_CD/Terraform/ec2.tf
+++ CI_CD/Terraform/ec2.tf
@@ -1,10 +1,20 @@
 resource "aws_instance" "jenkins" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Example AMI for Amazon Linux 2
+  ami           = "ami-0900fe555656695a2"  # AMI ID for Amazon Linux 2023
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.ci_subnet.id
+  key_name                = "CI_CD"                  # The key pair name
+  vpc_security_group_ids  = [aws_security_group.jenkins_sg.id]
+  subnet_id     = data.aws_subnet.default.id
   security_groups = [aws_security_group.jenkins_sg.name]
+  associate_public_ip_address = true
+
+  root_block_device {
+    volume_type = "gp3"
+    volume_size = 8
+  }
 
   tags = {
     Name = "Jenkins"
   }
+
 }
+
--- CI_CD/Terraform/security.tf
+++ CI_CD/Terraform/security.tf
@@ -1,14 +1,33 @@
 resource "aws_security_group" "jenkins_sg" {
-  name        = "jenkins_sg"
+  name        = "launch-wizard-1"
   description = "Security group for Jenkins CI"
-  vpc_id      = aws_vpc.ci_vpc.id
+  vpc_id      = data.aws_vpc.default.id
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "SSH"
   }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTP"
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTPS"
+  }
+
+
 
   egress {
     from_port   = 0
