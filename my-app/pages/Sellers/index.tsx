f1bead4d824ae6dbf8d95f7ae58fd0d9e8b73bc4
elizabeth-hrdz <elizabethhernandez5@csus.edu>
2024-04-21 10:47:48 -0700

    DP-104 Fix the navbar to display on all pages

--- my-app/pages/Sellers/index.tsx
+++ my-app/pages/Sellers/index.tsx
@@ -1,8 +1,9 @@
-
+import NavBar from "../../src/app/components/Navbar/navbar";
 
 const Sellers = () => {
     return (
       <div>
+        <NavBar />
         <h1>List for nearby houses in the same region of the location.</h1>
         <p> ask the user for their address. </p>
         <p> give an estimate house price for user's house , if the user enter they houes address . </p>
