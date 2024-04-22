1912487519ffb5226ab28e171aa5027e88648cdd
ShadeManEXE <jflores99@rocketmail.com>
2024-04-21 23:21:27 -0700

    DP-100 Fixed the format of the sellers page

--- my-app/pages/Sellers/index.tsx
+++ my-app/pages/Sellers/index.tsx
@@ -20,9 +20,9 @@ const Sellers = () => {
                 </div>
                 <NavBar/>
             </div>
-        <div>
+            <div>
             < StaticImageTechJAF/>
-        </div>
+            </div>
 
 
 
--- my-app/src/app/components/Static Image Tech JAF/Static Image Tech JAF.tsx
+++ my-app/src/app/components/Static Image Tech JAF/Static Image Tech JAF.tsx
@@ -6,12 +6,25 @@ const StaticImageTechJAF = ()  => {
     return (
 
         <div className="image_tech_frame">
-            <div
-                className="Frame25 w-96 h-96 pl-16 pr-96 pt-60 pb-56 bg-opacity-100 flex-col justify-end items-start gap-9 inline-flex">
+            <div className="IntroMainpage w-max h-max relative flex-col justify-items-center items center inline-flex">
                 <div
-                    className="Selling w-96 h-24 text-white text-8xl font-bold font-['Inter'] leading-10 tracking-widest">Selling
+                    className="LourdesMendoza text-white text-6xl font-bold font-['Inter'] leading-10 tracking-widest">Selling
                 </div>
-                <div className="PlaceHolderPhrase text-white text-lg font-bold font-['Inter'] text-wrap leading-10">Selling your home with me should bring you a sense of ease, certainty, and confidence
+                <div
+                    className="PoweredByBigBlockRealtyNorth text-white text-lg font-bold font-['Inter'] leading-10 tracking-widest">Selling your home with me should bring you a sense of ease, certainty, and confidence
+                </div>
+                <div
+                    className="LinkStylablebutton2545352419Root w-36 h-11 p-1.5 bg-neutral-800 border-4 border-white justify-center items-center inline-flex">
+                    <div
+                        className="DivStylablebutton2545352419Container grow shrink basis-0 self-stretch px-1.5 py-1 justify-center items-center inline-flex">
+                        <div
+                            className="SpanStylablebutton2545352419Label grow shrink basis-0 self-stretch py-px justify-center items-center inline-flex">
+                            <div
+                                className="ContactMe text-white text-base font-bold font-['Playfair Display'] leading-tight tracking-widest">Contact
+                                Me
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
