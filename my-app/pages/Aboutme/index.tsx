cd98c18b67605c2fade6ff12edd64b5db39f5827
julianpulido272 <julianpulido707@gmail.com>
2024-06-26 14:49:49 -0700

    fixed tailwind css directory

--- my-app/pages/Aboutme/index.tsx
+++ my-app/pages/Aboutme/index.tsx
@@ -2,15 +2,30 @@ import NavBar from "../../src/app/components/Navbar/navbar";
 import React from "react";
 import "../../src/app/globals.css";
 
+
 const Aboutme = () => {
   return (
     <div>
-      <NavBar />
-      <div > {/* Inline style for testing purposes */}
-        <p className="text-blue-600">hello world</p>
+      <NavBar></NavBar>
+      <div className="flex h-screen w-screen bg-gray-100">
+        <div >
+          <h1 className="text-8xl text-light-blue font-bold mb-29">
+            Hello, I'm Lourdes Mendoza
+
+          </h1>
+          <p className="text-transparent text-7xl font-bold pt-50">Local Sacramento Realtor</p>
+        </div>
+        <div className="flex justify-center items-center w-1/2">
+          <img
+            className="object-cover h-full w-full opacity-20"
+            src="https://www.stickpng.com/"
+            alt="Transparent background image"
+          />
+        </div>
       </div>
     </div>
   );
 };
 
 export default Aboutme;
+
