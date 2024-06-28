9a4da9d74ddd43c9d8f927420dd7b9b5a3a450a4
julianpulido272 <julianpulido707@gmail.com>
2024-06-28 15:28:40 -0700

    reconfigured navbar for mobile users

--- my-app/pages/Aboutme/index.tsx
+++ my-app/pages/Aboutme/index.tsx
@@ -2,30 +2,29 @@ import NavBar from "../../src/app/components/Navbar/navbar";
 import React from "react";
 import "../../src/app/globals.css";
 
-
 const Aboutme = () => {
   return (
-    <div>
-      <NavBar></NavBar>
-      <div className="flex h-screen w-screen bg-gray-100">
-        <div >
-          <h1 className="text-8xl text-light-blue font-bold mb-29">
-            Hello, I'm Lourdes Mendoza
-
+    <main className="container mx-auto px-4">
+      <header>
+        <NavBar />
+      </header>
+      <div className="flex flex-col md:flex-row md:items-center md:justify-center bg-gray-50">{/* flex flex col for mobile devices to stack columns. md for medium screens to place stuff by row */}
+        <div className="md:mr-20 md:mt-40">
+          <h1 className="text-4xl text-[#299FDD] font-bold">
+            Lourdes Mendoza
           </h1>
-          <p className="text-transparent text-7xl font-bold pt-50">Local Sacramento Realtor</p>
+          <p className="text-2xl">
+            Local Sacramento Realtor
+          </p>
         </div>
-        <div className="flex justify-center items-center w-1/2">
-          <img
-            className="object-cover h-full w-full opacity-20"
-            src="https://www.stickpng.com/"
-            alt="Transparent background image"
-          />
-        </div>
+        <img
+          className="object-contain w-full md:max-w-md md:mt-20"
+          src="/lourdes-removebg-preview.png"
+          alt="Transparent background image"
+        />
       </div>
-    </div>
+    </main>
   );
 };
 
 export default Aboutme;
-
--- my-app/src/app/components/Navbar/navbar.tsx
+++ my-app/src/app/components/Navbar/navbar.tsx
@@ -1,10 +1,8 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-//This style is the style of the buttons on the navbar
-//TODO: We need a better way to manage all styles so they are easily reusable in other pages without having to retype them
-const buttonStyle = "ml-2 bg-transparent hover:bg-gray-200 text-black font-bold py-1 px-2 rounded";
 
+const buttonStyle = "ml-2 bg-transparent hover:bg-gray-200 text-black font-bold py-1 px-2 rounded";
 
 function NavBar() {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
@@ -12,104 +10,164 @@ function NavBar() {
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
   }
-  
-    {/*Return a 'menu' icon for the navabar that collapses all our buttons. This is only for mobile users*/}
-    return (
-      
-      
-<div className="navbar-container">
-      
-    {/*Logo of the website*/}
-    <Link href="/" passHref>
+
+  return (
+    <nav className="navbar-container">
+      <div className=" mx-auto flex items-center">
+        {/* Logo */}
+        <Link href="/" passHref>
           <Image src="/logo_.png" alt="logo" width={150} height={150} priority />
         </Link>
-      {/* Spacing div to push content to the right */}
-      <div className="flex-grow"></div>
-      {/*Buttons for the navbar*/}
-        <Link href="/"> 
-          <button className={buttonStyle}>
-          Home
+
+        {/* Spacer */}
+        <div className="flex-grow"></div>
+
+        {/* Hamburger Icon for Mobile */}
+        <div className="text-black block lg:hidden">
+          <button
+            onClick={toggleDropdown}
+            className="text-black focus:outline-none"
+          >
+            <svg
+              className="h-6 w-6 fill-current"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                className={isDropdownOpen ? 'hidden' : 'block'}
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M3 5h18v2H3V5zm0 6h18v2H3v-2zm0 6h18v2H3v-2z"
+              />
+              <path
+                className={isDropdownOpen ? 'block' : 'hidden'}
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M4.293 6.293a1 1 0 011.414 0l6.293 6.293 6.293-6.293a1 1 0 111.414 1.414l-7 7a1 1 0 01-1.414 0l-7-7a1 1 0 010-1.414 1 1 0 010 0z"
+              />
+            </svg>
           </button>
-        </Link>
-
-
-
-      <Link href="/Listings">
-      <button className={buttonStyle}>
-        Listings
-      </button>
-      </Link>
-
-
-
-      <Link href="/Aboutme">
-        <button className={buttonStyle}>
-          About Me
-        </button>
-      </Link>
-      
-      <Link href="/Sellers">
-        <button className={buttonStyle}>
-          Sellers
-        </button>
-      </Link>
-
-
-
-      
-      <Link href="/Buyers">
-        <button className={buttonStyle}>
-          Buyers
-        </button>
-      </Link>
-
-
-      <Link href="/Communities">
-        <button className={buttonStyle}>
-          Communities
-        </button>
-      </Link>
-
-
-
-      <Link href="/Properties">
-      <button className={buttonStyle}>
-        Properties
-      </button>
-      </Link>
-
-
-
-      <Link  href="/WhatMyHomeWorth">
-      <button className={buttonStyle}>
-        What's My Home Worth?
-      </button>
-      </Link>
-
-
-      {/*<Link href="/Sign-in"*/}
-      {/*create dropdown menu*/}
-             <div className="md:hidden">
-          <button className={buttonStyle} onClick={toggleDropdown}>Menu</button>
         </div>
-      <div className= "dropdown button" >
-        <button className={buttonStyle} onClick={toggleDropdown}>
-          Login 
-        </button>
-        {/*dropdown menu*/}
-        {isDropdownOpen && (
-          <div className="absolute right-0 mt-2 bg-gray-200 rounded-md shadow-lg" >
-            <Link href="/Sign-in" className="block px-4 py-2 text-gray-800 hover:bg-gray-300">Sign In<a/>
-            </Link>
-            <Link href="/Sign-up" className="block px-4 py-2 text-gray-800 hover:bg-gray-300">Create account<a/>
-            </Link>
-            </div>
-            
-        )}
+
+        {/* Desktop Menu */}
+        <div className="hidden lg:flex lg:items-center lg:w-auto">
+          <Link href="/">
+            <button className={buttonStyle}>
+              Home
+            </button>
+          </Link>
+
+          <Link href="/Listings">
+            <button className={buttonStyle}>
+              Listings
+            </button>
+          </Link>
+
+          <Link href="/Aboutme">
+            <button className={buttonStyle}>
+              About Me
+            </button>
+          </Link>
+
+          <Link href="/Sellers">
+            <button className={buttonStyle}>
+              Sellers
+            </button>
+          </Link>
+
+          <Link href="/Buyers">
+            <button className={buttonStyle}>
+              Buyers
+            </button>
+          </Link>
+
+          <Link href="/Communities">
+            <button className={buttonStyle}>
+              Communities
+            </button>
+          </Link>
+
+          <Link href="/Properties">
+            <button className={buttonStyle}>
+              Properties
+            </button>
+          </Link>
+
+          <Link href="/WhatMyHomeWorth">
+            <button className={buttonStyle}>
+              What's My Home Worth?
+            </button>
+          </Link>
+
+          <Link href="/Sign-in">
+            <button className={buttonStyle}>
+              Sign in
+            </button>
+          </Link>
+        </div>
       </div>
 
-    </div>
-  
+      {/* Mobile Menu */}
+      <div className={`lg:hidden ${isDropdownOpen ? 'block' : 'hidden'}`}>
+        <div className="px-2 pt-2 pb-3">
+          <Link href="/">
+            <button className={buttonStyle}>
+              Home
+            </button>
+          </Link>
+
+          <Link href="/Listings">
+            <button className={buttonStyle}>
+              Listings
+            </button>
+          </Link>
+
+          <Link href="/Aboutme">
+            <button className={buttonStyle}>
+              About Me
+            </button>
+          </Link>
+
+          <Link href="/Sellers">
+            <button className={buttonStyle}>
+              Sellers
+            </button>
+          </Link>
+
+          <Link href="/Buyers">
+            <button className={buttonStyle}>
+              Buyers
+            </button>
+          </Link>
+
+          <Link href="/Communities">
+            <button className={buttonStyle}>
+              Communities
+            </button>
+          </Link>
+
+          <Link href="/Properties">
+            <button className={buttonStyle}>
+              Properties
+            </button>
+          </Link>
+
+          <Link href="/WhatMyHomeWorth">
+            <button className={buttonStyle}>
+              What's My Home Worth?
+            </button>
+          </Link>
+          
+          <Link href="/Sign-in">
+            <button className={buttonStyle}>
+              Sign in
+            </button>
+          </Link>
+
+        </div>
+      </div>
+    </nav>
   );
 }
+
 export default NavBar;
