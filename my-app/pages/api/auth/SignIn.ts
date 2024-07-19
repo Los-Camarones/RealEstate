15c4c31ed6190b18068c5ebde838312d2336f2ca
julianpulido272 <julianpulido707@gmail.com>
2024-07-18 20:06:29 -0700

    add sign-in api route

---
+++ my-app/pages/api/auth/SignIn.ts
@@ -0,0 +1,54 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import supabase from '../../../utils/supabase/supabaseClient';
+
+/**
+ * Calls Supabase to handle sign in feature for users
+ */
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse)
+{
+  if(req.method === 'POST')
+  {
+    //grab user's credentials from request
+    const{email, password} = req.body;
+
+    try
+    {
+      //call api sign in
+      const{data, error} = await supabase.auth.signInWithPassword({
+        email,
+        password
+      })
+
+      if(error)
+      {
+        //unauthorization error 401
+        return res.status(401).json({error: error.message});
+      }
+
+      //successful login 200 OK
+      return res.status(200).json({message: 'Success login in'});
+    } 
+    catch(error)
+    {
+      if(error instanceof(Error))
+      {
+        console.log('Error signing in' , error.message);
+      }
+      else
+      {
+        console.log(error);
+      }
+
+      //500 internal server error
+      return res.status(500).json({error: 'Internal server error'});
+
+    }
+    
+  }
+  else
+  {
+    res.setHeader('Allow', ['POST']);
+    return res.status(405).json({ error: `Method ${req.method} not allowed` });
+  }
+}
