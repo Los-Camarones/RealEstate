8708614a3a0ad67d02741fbae878f191c8f095c8
Eric <95461143+ERC-320-61@users.noreply.github.com>
2024-10-01 16:21:30 -0700

    Babel config was casuing build errrors, took it out

--- my-app/babel.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  presets: [
-    'next/babel', // Use the Next.js Babel preset
-    '@babel/preset-env',      // For transforming ES6+ JavaScript
-    '@babel/preset-react',    // For transforming JSX (React)
-    '@babel/preset-typescript', // For transforming TypeScript
-  ],
-};
