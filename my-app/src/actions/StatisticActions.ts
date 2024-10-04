2b9849f472e762e19e1deb222fa4dfc2f7bf7878
Julian Pulido <julianpulido707@gmail.com>
2024-10-04 14:54:59 -0700

    add plot chart with subscriber leads

--- my-app/src/actions/StatisticActions.ts
+++ my-app/src/actions/StatisticActions.ts
@@ -4,6 +4,7 @@
 "use server";
 
 import { createSupabaseServerClient } from "@/utils/supabase/supabaseServer";
+import { IStatistic } from "@/types/database_interface";
 
 /**
  * Function to insert the number of subscribers into the Statistics table
@@ -11,14 +12,16 @@ import { createSupabaseServerClient } from "@/utils/supabase/supabaseServer";
  * @returns {Promise<{ success: boolean; userId?: string; errorMessage?: string }>}
  *
  */
-export async function insertSubscriberCount(subscriberCount: number): Promise<{ success: boolean; error?: string }> {
+export async function insertSubscriberCount(
+  subscriberCount: number
+): Promise<{ success: boolean; error?: string }> {
   const supabase = createSupabaseServerClient();
 
   try {
     // Insert user into the custom Users schema
     const { data, error } = await supabase
       .from("Statistics")
-      .insert([{ type: "Subscribers", count: subscriberCount }])
+      .insert([{ type: "Subscribers", count: subscriberCount }]);
 
     if (error) {
       return {
@@ -27,6 +30,31 @@ export async function insertSubscriberCount(subscriberCount: number): Promise<{ 
       };
     } else {
       return { success: true };
+    }
+  } catch (error) {
+    console.error("Unexpected error inserting new user:", error);
+    return { success: false, error: "An unexpected error occurred." };
+  }
+}
+
+export async function getSubscribersRows(): Promise<{success: boolean;error?: string;data?: any;}> {
+  const supabase = createSupabaseServerClient();
+
+  try {
+    //read data from Statistics table
+    let { data: Statistics, error } = await supabase
+      .from("Statistics")
+      .select("*")
+      // Filters
+      .eq("type", "Subscribers");
+
+    if (error) {
+      return {
+        success: false,
+        error: "Error inserting subscriber count" + error.message,
+      };
+    } else {
+      return { success: true, data: Statistics };
     }
   } catch (error) {
     console.error("Unexpected error inserting new user:", error);
--- my-app/src/app/test/page.tsx
+++ my-app/src/app/test/page.tsx
@@ -1,31 +1,13 @@
 import React from 'react'
-import {getSubscriberCount} from '../../actions/HomeFinderActions' 
-import { insertSubscriberCount } from '@/actions/StatisticActions';
+import LineChart from '@/components/Dashboard/LineChart/LineChart'
 
-const page = async () => {
+const page = () => {
 
-    const response = await getSubscriberCount();
-
-    if (response.success && typeof response.subscriberCount === 'number') {
-      let subscriberNumber: number = response.subscriberCount;
-
-      const insertResponse = await insertSubscriberCount(subscriberNumber);
-
-      if (insertResponse.success) {
-        console.log("inserted subscriber successfully");
-      }
-      else {
-        console.log(insertResponse.error);
-      }
-
-    } else {
-      console.error("Subscriber count is undefined");
-    }
-    
   return (
 
     <div>
-        <h1>{response.subscriberCount}</h1>
+      <h1>testing page</h1>
+      <LineChart></LineChart>
     </div>
   )
 }
