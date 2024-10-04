17995a3c64fe669c0208f43867f6fdfe05133a0f
MANSOURCSS <Mohamedahmed2@CSUS.EDU>
2024-10-04 01:00:32 -0700

    Refactor import statement in page.tsx

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import NavBar from '../components/Navbar/navbar';
 import HomePageContent from '../components/HomePageContent/HomePageContent';
 import "./globals.css";
