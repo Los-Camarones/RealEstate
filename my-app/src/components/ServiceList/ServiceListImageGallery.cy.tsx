759e756a20298f6392e1b827b7ef428c7f889af4
elizabethhernandez5 <elizabethhernandez5@csus.edu>
2024-10-22 16:18:48 -0700

    service list update

--- my-app/src/components/ServiceList/ServiceListImageGallery.cy.tsx
+++ my-app/src/components/ServiceList/ServiceListImageGallery.cy.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import ImageGallery from './ServiceList';
 
+
 describe('<ImageGallery />', () => {
   beforeEach(() => {
     // Mount the component before each test
     cy.mount(<ImageGallery />);
   });
 
+
   it('renders the component', () => {
     // Ensure the main container is rendered
     cy.get('.image-container').should('exist');
   });
 
+
   it('renders the correct number of images', () => {
     // Check the number of images rendered
     cy.get('.image-wrapper').should('have.length', 4);
   });
+
 
   it('renders the correct captions', () => {
     // Check if each caption is rendered correctly
@@ -26,10 +30,12 @@ describe('<ImageGallery />', () => {
       'For Sale'
     ];
 
+
     captions.forEach((caption, index) => {
       cy.get('.caption').eq(index).should('contain.text', caption);
     });
   });
+
 
   it('renders the correct image sources', () => {
     // Ensure each image has the correct source (src attribute)
@@ -40,24 +46,28 @@ describe('<ImageGallery />', () => {
       '/picture4.jpg'
     ];
 
+
     imageSources.forEach((src, index) => {
       cy.get('.circular-image').eq(index).should('have.attr', 'src', src);
     });
   });
 
+
   it('has correct links for each image', () => {
     // Ensure that each image link has the correct href attribute
     const routes = [
-      '/WhatMyHomeWorth',
-      '/listings',
-      '/Sellers',
+      '/valuation',
+      '/property-search',
+      '/GetPreQualified',
       '/Sellers'
     ];
+
 
     routes.forEach((route, index) => {
       cy.get('.image-link').eq(index).should('have.attr', 'href', route);
     });
   });
+
 
   it('applies correct CSS classes', () => {
     // Check if the CSS classes are applied correctly to the images
@@ -65,3 +75,5 @@ describe('<ImageGallery />', () => {
     cy.get('.image-link').should('have.class', 'image-link');
   });
 });
+
+
