7da4cefe637cdbca135e2268e5b67071fcb85076
Julian Pulido <julianpulido707@gmail.com>
2024-10-22 13:13:12 -0700

    DP-261 Update users testimonials

--- my-app/src/actions/BucketActions.ts
+++ my-app/src/actions/BucketActions.ts
@@ -4,6 +4,7 @@
 'use server';
 import { createSupabaseServerClient } from "../utils/supabase/supabaseServer";
 import { decode } from 'base64-arraybuffer'
+import { randomUUID, UUID } from "crypto";
 
 
 
@@ -11,16 +12,21 @@ import { decode } from 'base64-arraybuffer'
  * Inserts a profile picture to the bucket storage
  */
 
-export async function uploadProfilePicture(URL_image: string) {
+export async function uploadProfilePicture(image_name: string, file_image: string) {
 
   try {
     const supabase = createSupabaseServerClient();
+
+    //generate a UUID to attach the image
+    const uuid = randomUUID();
+
+    const new_image_path = image_name+uuid;
 
     //upload file to supabase bucket
     const { data, error } = await supabase
     .storage
     .from('testimonials_images')
-    .upload(URL_image, decode('base64FileData'), {
+    .upload(`${new_image_path}`, decode(file_image), {
       contentType: 'image/png'
   })
 
@@ -37,7 +43,30 @@ export async function uploadProfilePicture(URL_image: string) {
     console.error('Unexpected error fetching user information:', error);
     return {success: false, error: 'An unexpected error occurred.' };  
   }
+}
 
+/**
+ * Function to get the signed URL of a profile picture
+ * @param image_path 
+ */
+export async function getPublicURL(image_name: string) {
+  try {
+    const supabase = createSupabaseServerClient();
 
+    const { data } = supabase
+    .storage
+    .from('testimonials_images')
+    .getPublicUrl(`${image_name}`)
 
+    if(data) {
+      return{success: true, data: data}
+    } else {
+      return {success: false, error: 'Could not retrieve URL to profile picture'};
+
+    }
+
+  } catch (error) {
+    console.error('Unexpected error fetching profile picture URL:', error);
+    return {success: false, error: 'An unexpected error occurred.' };  
+  }
 }
