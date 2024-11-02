dbe180636c117af5c94f9a3d13aeb122e087a69e
LushRamen <wendyle@csus.edu>
2024-11-01 22:47:53 -0700

    src/app/Aboutme/about.module.css 	src/app/Admin/market-report-signup-requests/ 	src/app/Admin/open-home-report-signup-requests/ 	src/app/Admin/property-details/ 	src/app/Listings/ 	src/app/Sign-up/ 	src/app/api/contact/ 	src/app/api/marketReportSignupRequests/ 	src/app/api/openHomeReportSignupRequests/ 	src/app/api/valuation-requests/ 	src/components/Modal/ 	src/components/SignUp/

--- my-app/src/components/Admin/Sidebar.tsx
+++ my-app/src/components/Admin/Sidebar.tsx
@@ -32,8 +32,8 @@ const Sidebar: React.FC = () => {
         { name: "Request Showing", path: "/Admin/scheduleShowingRequests" },
         { name: "Valuation Requests", path: "/Admin/valuation-requests" },
         { name: "More Info Requests", path: "/Admin/more-info-requests"},
-        { name: "Market Sign up Request", path: "/Admin/marketReportSignupRequest"},
-        { name: "Open Home Report Signup Requests", path:"/Admin/openHomeSignupRequests"},
+        { name: "Market Sign up Request", path: "/Admin/market-report-signup-requests"},
+        { name: "Open Home Report Signup Requests", path:"/Admin/open-home-report-signup-requests"},
         { name: "Schedule Showing Requests", path: "/Admin/scheduleShowingRequests" },
         {name: "Email Update Sign-up Requests", path: "/Admin/email-updates"}
       ],
